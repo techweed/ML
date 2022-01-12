94bdaed4a59e6ab077dd953edc7d186e07a31d7a
Joseph Antony <techweed@users.noreply.github.com>
2022-01-12 13:09:08 +0530

    task completed

--- Task1_Python_script.py
+++ Task1_Python_script.py
@@ -23,7 +23,7 @@ def get_page():
 		sys.exit(1)
 
 	# Code here - Call get method in requests object, pass url and collect it in res
-	
+	res = requests.get(url)
 	# Code ends here
 
 	res.raise_for_status()
@@ -57,8 +57,8 @@ def save_file(text):
 	fname = f'scraped_articles/{name}.txt'
 	
 	# Code here - write a file using with (2 lines)
-	
-
+	with open(fname, 'w') as f:
+		f.write(text)
 	# Code ends here
 
 	print(f'File saved in directory {fname}')
